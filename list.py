2b4818e54843cbeb3343c36b63676c1b14877655
Manisha_Singh <manishakumari5nov@gmail.com>
2026-07-17 06:53:31 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+a=[1,2,3,5,7,9]
+b=["apple","boy","kiwi","mango"]
+print(a)
+print(a[1:4])
+print(a[:3])
+print(a[2:4])
+print(a[1:])
+print(a[:-1])#last index is in include
+print(len(a))
+print(a[0])
+a[2]="ram"
+print(a)
+print(type(a))
+c=list()#empty list
+print(c)
+a[1:3]=10,50,80
+print(a)
